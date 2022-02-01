eb065a4982914c2fd805c7222cc3742dd0752540
recurator <bilekmirko@gmail.com>
2022-02-01 16:22:56 +0100

    Perform async Github users download

---
+++ app/workers/github_download_users_worker.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class GithubDownloadUsersWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: :critical, unique: :until_executed
+
+  def perform(users)
+    users.each do |user|
+      if user.type == "Organization"
+        RepositoryOrganisation.where(host_type: "GitHub").find_or_create_by(uuid: user.id) do |db_user|
+          db_user.login = user.login
+        end
+      else
+        RepositoryUser.create_from_host("GitHub", user)
+      end
+    rescue
+      nil
+    end
+  end
+end
