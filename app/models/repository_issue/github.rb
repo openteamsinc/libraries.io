0a1d6dcfd0a0ed153a748acb6457b9ad6a6e2918
recurator <bilekmirko@gmail.com>
2022-02-25 11:19:33 +0100

    Cache repository/repository_user query

--- app/models/repository_issue/github.rb
+++ app/models/repository_issue/github.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
+
 module RepositoryIssue
   class Github < Base
-
     def url
       path = issue.pull_request ? 'pull' : 'issues'
       "#{issue.repository.url}/#{path}/#{issue.number}"
@@ -19,18 +19,28 @@ module RepositoryIssue
 
     def self.create_from_hash(name_with_owner, issue_hash, token = nil)
       return if issue_hash.nil?
+
       issue_hash = issue_hash.to_hash
-      repository = Repository.host('GitHub').find_by_full_name(name_with_owner) || RepositoryHost::Github.create(name_with_owner)
-      return if repository.nil?
-      i = repository.issues.find_or_create_by(uuid: issue_hash[:id])
-      user = RepositoryUser.where(host_type: 'GitHub').find_by_uuid(issue_hash[:user][:id]) || RepositoryOwner::Github.download_user_from_host('GitHub', issue_hash[:user][:login]) rescue nil
-      i.repository_user_id = user.id if user.present?
-      i.repository_id = repository.id
+      repository_id = Rails.cache.fetch("repository-#{name_with_owner}", expires_in: 15.minutes) do
+        Repository.host("GitHub").find_by_full_name(name_with_owner)&.id || RepositoryHost::Github.create(name_with_owner)&.id
+      end
+      return unless repository_id
+
+      i = Issue.where(repository_id: repository_id).find_or_create_by(uuid: issue_hash[:id])
+
+      user_id = Rails.cache.fetch("repository_user-#{issue_hash[:user][:id]}", expires_in: 15.minutes) do
+        RepositoryUser.where(host_type: "GitHub").find_by_uuid(issue_hash[:user][:id])&.id || RepositoryOwner::Github.download_user_from_host("GitHub", issue_hash[:user][:login])&.id rescue nil
+      end
+
+      i.repository_user_id = user_id if user_id
+      i.repository_id = repository_id
       i.labels = issue_hash[:labels].map{|l| l[:name] }
       i.pull_request = issue_hash[:pull_request].present?
       i.comments_count = issue_hash[:comments]
       i.host_type = 'GitHub'
-      i.assign_attributes issue_hash.slice(:number, :state, :title, :body, :locked, :closed_at, :created_at, :updated_at)
+      i.body = issue_hash[:body]&.gsub(/\0/, '')
+      i.title = issue_hash[:title]&.gsub(/\0/, '')
+      i.assign_attributes issue_hash.slice(:number, :state, :locked, :closed_at, :created_at, :updated_at)
       if i.changed?
         i.last_synced_at = Time.now
         i.save!
